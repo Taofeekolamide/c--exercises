d3b2e8fcb7c01a9d7105af902bf9f049418f01da
Taofeekolamide <taofeekolamide55@gmail.com>
2024-12-25 16:00:58 +0100

    update

---
+++ FourthChapter/Program.cs
@@ -0,0 +1,127 @@
+﻿/*Write an if-statement that takes two integer variables and exchanges
+their values if the first one is greater than the second one.
+Console.Write("Enter int a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter int b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    int c = a;
+    a = b;
+    b = c;
+    Console.WriteLine($"{a} {b}");
+}*/
+
+/*2. Write a program that shows the sign (+ or -) of the product of three real
+numbers, without calculating it. Use a sequence of if operators.
+Console.Write("Enter integer: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 0) {
+    Console.Write($"+{a}");
+} else {
+    Console.Write($"-{a}");
+}*/
+
+/*3. Write a program that finds the biggest of three integers, using nested
+if statements.
+Console.Write("Enter int a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter int b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter int c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+*/
+
+/*4. Sort 3 real numbers in descending order. Use nested if statements.*/
+
+/*5. Write a program that asks for a digit (0-9), and depending on the input,
+shows the digit as a word (in English). Use a switch statement.
+
+Console.Write("Enter int between 0-9: ");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a) {
+    case 0: Console.Write("zero"); break;
+    case 1: Console.Write("one"); break;
+    case 2: Console.Write("two"); break;
+    case 3: Console.Write("three"); break;
+    case 4: Console.Write("four"); break;
+    case 5: Console.Write("five"); break;
+    case 6: Console.Write("six"); break;
+    case 7: Console.Write("seven"); break;
+    case 8: Console.Write("eight"); break;
+    case 9: Console.Write("nine"); break;
+    default: Console.Write("enter number between 0 to 9"); break;
+}*/
+
+/*6. Write a program that gets the coefficients a, b and c of a quadratic 
+equation: ax2 + bx + c, calculates and prints its real roots (if they exist). 
+Quadratic equations may have 0, 1 or 2 real roots. */
+
+/*7. Write a program that finds the greatest of given 5 numbers.
+Console.Write("Enter integer one: ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer two: ");
+int two = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer three: ");
+int threee = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer four: ");
+int four = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer five: ");
+int five = Convert.ToInt32(Console.ReadLine());
+
+int greatest = one;
+if(two > greatest) {
+    greatest = two;
+}
+if(threee > greatest) {
+    greatest = threee;
+}
+if (four > greatest) {
+    greatest = four;
+}
+if(five > greatest) {
+    greatest = five;
+}
+Console.WriteLine($"The greatest is integer one {greatest}");*/
+
+/*8. Write a program that, depending on the user’s choice, inputs int, double
+or string variable. If the variable is int or double, the program
+increases it by 1. If the variable is a string, the program appends "*" at
+the end. Print the result at the console. Use switch statement.*/
+
+
+/*9. We are given 5 integer numbers. Write a program that finds those
+subsets whose sum is 0. Examples:
+- If we are given the numbers {3, -2, 1, 1, 8}, the sum of -2, 1 and 1
+is 0.
+- If we are given the numbers {3, 1, -7, 35, 22}, there are no subsets
+with sum 0.*/
+
+
+/*10. Write a program that applies bonus points to given scores in the range
+[1…9] by the following rules:
+
+Console.Write("Enter integer score: ");
+int score = Convert.ToInt32(Console.ReadLine());
+if (score > 0 && score < 4) {
+    Console.WriteLine($"{score * 10}");
+} else
+if (score > 3 && score < 7) {
+    Console.WriteLine($"{score * 100}");
+}else
+if (score > 6 && score < 10) {
+    Console.WriteLine($"{score * 1000}");
+}else {
+    Console.WriteLine("error");
+}*/
+
+
+/*11. * Write a program that converts a number in the range [0…999] to
+words, corresponding to the English pronunciation. Examples:
+- 0 --> "Zero"
+- 12 --> "Twelve"
+- 98 --> "Ninety eight"
+- 273 --> "Two hundred seventy three"
+- 400 --> "Four hundred"
+- 501 --> "Five hundred and one"
+- 711 --> "Seven hundred and eleven"
+*/
--- ThirdChapter/Program.cs
+++ ThirdChapter/Program.cs
@@ -51,34 +51,120 @@ on the console. Each column should have a width of 10 characters and
 the numbers should be left aligned. The first number should be an
 integer in hexadecimal; the second should be fractional positive; and
 the third – a negative fraction. The last two numbers have to be
-rounded to the second decimal place.*/
+rounded to the second decimal place.
 
 Console.Write("Enter int one: ");
 int one = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter int two: ");
-int two = Convert.ToInt32(Console.ReadLine());
+double two = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter int three: ");
-int three = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"{one:x} {two} {three}");
+double three = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{one,-10:X} {two,-10:F2} {three,-10:F2}");*/
 
 
 /*5. Write a program that reads from the console two integer numbers (int)
 and prints how many numbers between them exist, such that the
 remainder of their division by 5 is 0. Example: in the range (14, 25)
-there are 3 such numbers: 15, 20 and 25.*/
+there are 3 such numbers: 15, 20 and 25.
+
+Console.Write("Enter integer one: ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer two: ");
+int two = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+for (int i = 14; i <= two; i++) {
+    if (i % 5 == 0) {
+        result += 1;
+    }
+}
+Console.WriteLine(result);*/
+
 /*6. Write a program that reads two numbers from the console and prints the
 greater of them. Solve the problem without using conditional
-statements.*/
+statements.
+Console.Write("Enter integer one: ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer two: ");
+int two = Convert.ToInt32(Console.ReadLine());
+bool greater = (one > two);
+Console.Write(greater);*/
+
 /*7. Write a program that reads five integer numbers and prints their
 sum. If an invalid number is entered the program should prompt the user
-to enter another number.*/
+to enter another number.
+Console.Write("Enter integer one: ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer two: ");
+int two = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer three: ");
+int threee = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer four: ");
+int four = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer five: ");
+int five = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"the result is {one+two+threee+four+five}");*/
+
 /*8. Write a program that reads five numbers from the console and prints the
-greatest of them.*/
+greatest of them.
+Console.Write("Enter integer one: ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer two: ");
+int two = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer three: ");
+int threee = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer four: ");
+int four = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter integer five: ");
+int five = Convert.ToInt32(Console.ReadLine());
+
+int greatest = one;
+if(two > greatest) {
+    greatest = two;
+}
+if(threee > greatest) {
+    greatest = threee;
+}
+if (four > greatest) {
+    greatest = four;
+}
+if(five > greatest) {
+    greatest = five;
+}
+Console.WriteLine($"The greatest is integer one {greatest}");*/
+
 /*9. Write a program that reads an integer number n from the console. After
-that reads n numbers from the console and prints their sum.*/
+that reads n numbers from the console and prints their sum.
+Console.Write("Enter integer n: ");
+int one = Convert.ToInt32(Console.ReadLine());
+int total = 0;
+for (int i = 0; i < one; i++) {
+    Console.Write("Enter integer x: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    total = total + x;
+}
+Console.Write(total);*/
+
 /*10. Write a program that reads an integer number n from the console and
-prints all numbers in the range [1…n], each on a separate line.*/
+prints all numbers in the range [1…n], each on a separate line.
+Console.Write("Enter integer n: ");
+int one = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= one; i++) {
+    Console.WriteLine(i);
+}*/
+
 /*11. Write a program that prints on the console the first 100 numbers in the
-Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, …*/
+Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, …
+int a = 0;
+int b = 1;
+
+Console.WriteLine($"{a}\n{b}");
+for (int i = 2; i < 98; i++) {
+    int next = (a+b);
+    Console.WriteLine(next);
+    a = b;
+    b = next;
+}*/
+
 /*12. Write a program that calculates the sum (with precision of 0.001) of
 the following sequence: 1 + 1/2 - 1/3 + 1/4 - 1/5 + …*/
