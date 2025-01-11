84ecbeda1682c4285f260465affaf434d1c59ed2
Taofeekolamide <taofeekolamide55@gmail.com>
2025-01-11 20:57:14 +0100

    update

--- SixthChapter/Program.cs
+++ SixthChapter/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] matrix = {
+    {1, 2, 3, 4, 5},
+    {2, 3, 4, 5, 6},
+    {3, 4, 5, 6, 7}
+};
+
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0)-1; i++) {
+    for (int j = 0; j < matrix.GetLength(1)-1; j++) {
+        int miniSum = matrix[i,j] + matrix[i,j+1] + matrix[i+1,j] + matrix[i+1,j+1];
+        Console.WriteLine(miniSum);
+        if (miniSum > sum) {
+            sum = miniSum;
+        }
+    }
+}
+Console.WriteLine(sum);
